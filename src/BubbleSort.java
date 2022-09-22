0ca04e25876fe94ec50a4b4cbfe3e9ac36fdb14f
DELL <DELL@10.12.40.69>
2022-09-22 14:20:20 +0530

    Sorted by Bubble Sort Algorithm

---
+++ src/BubbleSort.java
@@ -0,0 +1,34 @@
+
+public class BubbleSort {
+	public static void bubbleSort(int [] sort_arr, int len){
+        
+        for (int i=0;i<len-1;++i){
+
+            for(int j=0;j<len-i-1; ++j){
+
+                if(sort_arr[j+1]<sort_arr[j]){
+
+                    int swap = sort_arr[j];
+                    sort_arr[j] = sort_arr[j+1];
+                    sort_arr[j+1] = swap;
+
+                }
+            }
+        }
+	}
+	
+	
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int [] array = {1,5,10,15,20,25};
+        int len = array.length;
+        bubbleSort(array,len);
+
+        for(int i = 0; i<len; ++i){
+            System.out.print(array[i] + " ");
+        }
+	}
+}
+
+
